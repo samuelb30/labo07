cb55df4029b98030c4d510846f681c4e499f133c
samuelb30 <2351016@csfoy.ca>
2025-09-26 15:47:58 -0400

    Added movement

--- Assets/Scripts/Character/JammoPlayer.cs
+++ Assets/Scripts/Character/JammoPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class JammoPlayer : MonoBehaviour
@@ -9,6 +10,43 @@ public class JammoPlayer : MonoBehaviour
 
     [Header("Inputs")]
     [SerializeField] private InputActionReference moveAction;
+    private CharacterController characterController;
 
-    // TODO : Compléter cette classe.
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    private void Update()
+    {
+        var camera = Camera.main!;
+        var cameraTransform = camera.transform;
+        var forward = cameraTransform.forward;
+        var right = cameraTransform.right;
+
+
+        forward.y = 0;
+        right.y = 0;
+
+        var moveInput = moveAction.action.ReadValue<Vector2>();
+
+        if (moveInput == Vector2.zero)
+        {
+            characterController.Move(Vector2.zero);
+        }
+        else
+        {
+            var moveDirection = forward * moveInput.y + right * moveInput.x;
+            characterController.Move(moveDirection * (speed * Time.deltaTime));
+
+
+            var lookRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
+        }
+
+        
+        
+    }
 }
