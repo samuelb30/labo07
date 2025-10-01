dd366b53ef6bfe9372c3480781c6224e972584ad
samuelb30 <2351016@csfoy.ca>
2025-10-01 08:25:43 -0400

    ajout du saut

--- Assets/Scripts/Character/JammoPlayer.cs
+++ Assets/Scripts/Character/JammoPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Unity.VisualScripting;
+﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,10 +8,14 @@ public class JammoPlayer : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float speed = 10f;
     [SerializeField] private float rotationSpeed = 0.1f;
+    [SerializeField] private float jumpHeight = 2;
 
     [Header("Inputs")]
     [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+   
     private CharacterController characterController;
+    private float verticalVelocity;
 
     private void Awake()
     {
@@ -25,27 +30,45 @@ public class JammoPlayer : MonoBehaviour
         var cameraTransform = camera.transform;
         var forward = cameraTransform.forward;
         var right = cameraTransform.right;
-
+        var up = cameraTransform.up;
 
         forward.y = 0;
         right.y = 0;
 
         var moveInput = moveAction.action.ReadValue<Vector2>();
 
-        if (moveInput == Vector2.zero)
-        {
-            characterController.Move(Vector2.zero);
-        }
-        else
+        var horizontalMovement = Vector3.zero;
+        var verticalMovement = Vector3.zero;
+
+        if (moveInput != Vector2.zero)
         {
             var moveDirection = forward * moveInput.y + right * moveInput.x;
-            characterController.Move(moveDirection * (speed * Time.deltaTime));
+            horizontalMovement = moveDirection * (speed * Time.deltaTime);
 
 
             var lookRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
         }
 
+        var gravity = Physics.gravity;
+        var isGrounded = characterController.isGrounded;
+
+        if (isGrounded)
+        {
+            verticalVelocity = 0;
+
+            if (jumpAction.action.triggered)
+            {
+                verticalVelocity = Mathf.Sqrt(2 * jumpHeight * -gravity.y);
+            }
+        }
+
+        verticalVelocity += gravity.y * Time.deltaTime;
+
+        verticalMovement = up * (verticalVelocity * Time.deltaTime);
+
+        characterController.Move(verticalMovement + horizontalMovement);
+        
         
         
     }
